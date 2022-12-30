36da0c1b636b83235605da8432accb1d2cfdefd6
tiagocoelhoo <trcoelho@ua.pt>
2022-12-30 14:17:28 +0000

    chat and locations done

--- projeto/app/src/main/java/com/example/savenight/MainActivity.kt
+++ projeto/app/src/main/java/com/example/savenight/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.savenight
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,32 +20,176 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Maps())
+        //clean local storage
+        var sharedPref = getSharedPreferences("locations", Context.MODE_PRIVATE)
+        sharedPref.edit().clear().apply()
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.map -> {
-                    replaceFragment(Maps())
+                    //get the last fragment in the backstack
+                    val index = supportFragmentManager.backStackEntryCount - 1
+                    Log.d("index", index.toString())
+                    if (index >= 0) {
+                        val lastFragment = supportFragmentManager.getBackStackEntryAt(index)
+
+
+                        if (lastFragment.name== "lobby") {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            //hide the lobby fragment
+                            transaction.hide(supportFragmentManager.findFragmentByTag("lobby")!!)
+                            transaction.add(R.id.frameLayout, Maps())
+                            transaction.addToBackStack("map")
+
+                            transaction.commit()
+
+                        }
+                        else {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            transaction.hide(supportFragmentManager.fragments.last())
+                            transaction.add(R.id.frameLayout, Maps())
+                            transaction.addToBackStack("map")
+                            transaction.commit()
+
+
+                        }
+
+                    }else{
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frameLayout, Maps())
+                        transaction.addToBackStack("map")
+                        //hide actual fragment
+                        transaction.hide(supportFragmentManager.fragments.last())
+                        transaction.commit()
+                    }
                     true
                 }
                 R.id.feed -> {
-                    replaceFragment(Feed())
+                    //get the last fragment in the backstack
+                    val index = supportFragmentManager.backStackEntryCount - 1
+                    Log.d("index", index.toString())
+                    if (index >= 0) {
+                        val lastFragment = supportFragmentManager.getBackStackEntryAt(index)
+
+
+                        if (lastFragment.name == "lobby") {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            //hide the lobby fragment
+                            transaction.hide(supportFragmentManager.findFragmentByTag("lobby")!!)
+                            transaction.add(R.id.frameLayout, Feed())
+                            transaction.addToBackStack("feed")
+
+                            transaction.commit()
+                            return@setOnItemSelectedListener true
+                        } else {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            transaction.add(R.id.frameLayout, Feed())
+                            transaction.addToBackStack("feed")
+                            //hide actual fragment
+                            transaction.hide(supportFragmentManager.fragments.last())
+                            transaction.commit()
+
+                            return@setOnItemSelectedListener true
+                        }
+
+                    }else{
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frameLayout, Feed())
+                        transaction.addToBackStack("feed")
+                        transaction.commit()
+                    }
                     true
                 }
                 R.id.drunk -> {
-                    replaceFragment(DrunkTest())
+                    //get the last fragment in the backstack
+                    val index = supportFragmentManager.backStackEntryCount - 1
+                    Log.d("index", index.toString())
+                    if (index >= 0) {
+                        val lastFragment = supportFragmentManager.getBackStackEntryAt(index)
+
+
+                        if (lastFragment.name== "lobby") {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            //hide the lobby fragment
+                            transaction.hide(supportFragmentManager.findFragmentByTag("lobby")!!)
+                            transaction.add(R.id.frameLayout, DrunkTest())
+                            transaction.addToBackStack("drunk")
+
+                            transaction.commit()
+                            return@setOnItemSelectedListener true
+                        }
+                        else {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            transaction.add(R.id.frameLayout, DrunkTest())
+                            transaction.addToBackStack("drunktest")
+                            //hide actual fragment
+                            transaction.hide(supportFragmentManager.fragments.last())
+                            transaction.commit()
+
+                            return@setOnItemSelectedListener true
+                        }
+
+                    }else{
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frameLayout, DrunkTest())
+                        transaction.addToBackStack("drunktest")
+                        transaction.commit()
+                    }
                     true
                 }
                 R.id.camera -> {
-                    replaceFragment(Camera())
+                    //get the last fragment in the backstack
+                    val index = supportFragmentManager.backStackEntryCount - 1
+                    Log.d("index", index.toString())
+                    if (index >= 0) {
+                        val lastFragment = supportFragmentManager.getBackStackEntryAt(index)
+
+
+                        if (lastFragment.name== "lobby") {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            //hide the lobby fragment
+                            transaction.hide(supportFragmentManager.findFragmentByTag("lobby")!!)
+                            transaction.addToBackStack("camera")
+                            transaction.add(R.id.frameLayout, Camera())
+
+                            transaction.commit()
+                            return@setOnItemSelectedListener true
+                        }
+                        else {
+                            val transaction = supportFragmentManager.beginTransaction()
+                            transaction.add(R.id.frameLayout, Camera())
+                            //hide actual fragment
+                            transaction.hide(supportFragmentManager.fragments.last())
+
+
+                            transaction.addToBackStack("camera")
+                            transaction.commit()
+
+                            return@setOnItemSelectedListener true
+                        }
+
+                    }else{
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frameLayout, Camera())
+                        transaction.addToBackStack("camera")
+                        transaction.commit()
+                    }
                     true
                 }
                 R.id.group -> {
                     // see if whe have other fragment in the backstack
-                    val fragment = supportFragmentManager.findFragmentByTag("Lobby")
+                    val fragment = supportFragmentManager.findFragmentByTag("lobby")
                     Log.d("TAG", "onCreate: $fragment")
                     if (fragment != null) {
-                        //go to that fragment
-                        replaceFragment(fragment)
+                        Log.d("Lobby found", "onCreate: $fragment")
+                        // show the lobby fragment
+                        val transaction = supportFragmentManager.beginTransaction()
+                        //hide actual fragment
+                        transaction.hide(supportFragmentManager.fragments.last())
+                        transaction.show(fragment)
+                        transaction.addToBackStack("lobby")
+
+                        transaction.commit()
                     } else {
                         replaceFragment(Groups())
                     }
